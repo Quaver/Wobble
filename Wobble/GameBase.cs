db59011b2d6c9468ec74294f6e03682505fc00a8
Swan <me@swan.moe>
2018-07-22 19:58:11 -0400

    Add XML documentation to GameBase

--- Wobble/GameBase.cs
+++ Wobble/GameBase.cs
@@ -2,10 +2,14 @@
 
 namespace Wobble
 {
+    /// <summary>
+    ///     Contains all global objects needed for references throughout the game's life.
+    /// </summary>
     public static class GameBase
     {
         /// <summary>
-        ///     Contains a reference to the game.
+        ///     Contains a reference to the game itself. It is a singleton, so there can only
+        ///     ever be one game in existence.
         /// </summary>
         private static WobbleGame _game;
         public static WobbleGame Game
