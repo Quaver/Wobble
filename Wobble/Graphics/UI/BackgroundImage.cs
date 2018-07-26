6d2beff3d4480d9303bb6380c08e74ae231e6d84
Swan <me@swan.moe>
2018-07-26 10:14:03 -0400

    Add parallax option to BackgroundImage

--- Wobble/Graphics/UI/BackgroundImage.cs
+++ Wobble/Graphics/UI/BackgroundImage.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Wobble.Assets;
 using Wobble.Graphics.Sprites;
+using Wobble.Input;
 using Wobble.Window;
 
 namespace Wobble.Graphics.UI
@@ -33,12 +34,19 @@ namespace Wobble.Graphics.UI
             }
         }
 
+        /// <summary>
+        ///     Determines if we this background image has a parallax effect
+        ///     when moving the mouse cursor.
+        /// </summary>
+        public bool HasParallaxEffect { get; set; }
+
         /// <inheritdoc />
         /// <summary>
         /// </summary>
         /// <param name="image">The background image to use.</param>
+        /// <param name="hasParallaxEffect">If the background will move when the mouse cursor does.</param>
         /// <param name="dim">The background dim as a percentage from 1-100%</param>
-        public BackgroundImage(Texture2D image, int dim = 0)
+        public BackgroundImage(Texture2D image, int dim = 0, bool hasParallaxEffect = true)
         {
             Image = image;
             Size = new ScalableVector2(WindowManager.VirtualScreen.X + 100, WindowManager.VirtualScreen.Y + 100);
@@ -49,14 +57,24 @@ namespace Wobble.Graphics.UI
                 Tint = Color.Black,
                 Parent = this,
                 Size = Size,
-                Alpha = dim / 100f
             };
 
             Dim = dim;
+            HasParallaxEffect = hasParallaxEffect;
 
             // Hook onto the event when the resolution changes, so the background's size can change
             // accordingly.
             WindowManager.ResolutionChanged += OnResolutionChanged;
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            PerformParallaxEffect();
+            base.Update(gameTime);
         }
 
         /// <inheritdoc />
@@ -79,6 +97,21 @@ namespace Wobble.Graphics.UI
         {
             Size = new ScalableVector2(WindowManager.VirtualScreen.X + 100, WindowManager.VirtualScreen.Y + 100);
             BrightnessSprite.Size = Size;
+        }
+
+        /// <summary>
+        ///     Performs a parallax effect on the background if specified.
+        /// </summary>
+        private void PerformParallaxEffect()
+        {
+            if (!HasParallaxEffect)
+                return;
+
+            // Parallax
+            var mousePos = MouseManager.CurrentState.Position;
+
+            Y = (mousePos.Y - WindowManager.VirtualScreen.Y / 2f) / 60f - 50;
+            X = (mousePos.X - WindowManager.VirtualScreen.X / 2f) / 60f - 50;
         }
     }
 }
