a9166b25769415777dfb26a0355aaad80a828112
Swan <me@swan.moe>
2018-08-16 13:25:17 -0400

    Add BlurContainer

---
+++ Wobble/Graphics/Sprites/BlurContainer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Wobble.Assets;
+using Wobble.Graphics.Shaders;
+
+namespace Wobble.Graphics.Sprites
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Container for blurring sprites. Any child element will be placed under the same blur effect.
+    /// </summary>
+    public class BlurContainer : RenderTargetContainer
+    {
+        /// <summary>
+        ///     The type of blur to give it.
+        /// </summary>
+        public BlurType BlurType { get; set; }
+
+        /// <summary>
+        ///     The types of blur effects.
+        /// </summary>
+        private Dictionary<BlurType, Effect> BlurEffects { get; }
+
+        /// <summary>
+        ///     The strength of the blur.
+        /// </summary>
+        public float Strength { get; set; }
+
+        /// <summary>
+        ///      Creates a new blur container
+        /// </summary>
+        public BlurContainer(BlurType blurType, float strength)
+        {
+            // ReSharper disable once ArrangeConstructorOrDestructorBody
+            SpriteBatchOptions = new SpriteBatchOptions()
+            {
+                SortMode = SpriteSortMode.Deferred,
+                BlendState = BlendState.NonPremultiplied,
+                SamplerState = SamplerState.PointClamp,
+                DepthStencilState = DepthStencilState.Default,
+                RasterizerState = RasterizerState.CullNone,
+            };
+
+            // Load all three shaders.
+            BlurEffects = new Dictionary<BlurType, Effect>
+            {
+                {BlurType.Gaussian, new Effect(GameBase.Game.GraphicsDevice, WobbleResources.gaussian_blur)},
+                {BlurType.Frosty, new Effect(GameBase.Game.GraphicsDevice, WobbleResources.frosty_blur)},
+                {BlurType.Fast, new Effect(GameBase.Game.GraphicsDevice, WobbleResources.fast_blur)},
+            };
+
+            BlurType = blurType;
+            Strength = strength;
+
+            SpriteBatchOptions.Shader = new Shader(BlurEffects[BlurType], new Dictionary<string, object>
+            {
+                {"p_blurValues", new Vector3(Width, Height, Strength)}
+            });
+        }
+
+        /// <inheritdoc />
+        ///  <summary>
+        ///  </summary>
+        ///  <param name="gameTime"></param>
+        public override void Draw(GameTime gameTime)
+        {
+            // Set to the correct blur type.
+            SpriteBatchOptions.Shader.ShaderEffect = BlurEffects[BlurType];
+
+            // Set dictionary parameters with updated properties.
+            SpriteBatchOptions.Shader.SetParameter("p_blurValues", new Vector3(Width, Height, Strength), true);
+
+            base.Draw(gameTime);
+        }
+    }
+
+    /// <summary>
+    ///     Enum containing the different types of blur.
+    /// </summary>
+    public enum BlurType
+    {
+        Gaussian,
+        Frosty,
+        Fast
+    }
+}
