f36a31eef3edaf9afbfd2995424b60e70bd7ba35
Swan <me@swan.moe>
2019-02-17 13:12:49 -0500

    Make ButtonManager public

--- Wobble/Graphics/UI/Buttons/ButtonManager.cs
+++ Wobble/Graphics/UI/Buttons/ButtonManager.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace Wobble.Graphics.UI.Buttons
 {
-    internal static class ButtonManager
+    public static class ButtonManager
     {
         /// <summary>
         ///     The list of buttons that are currently drawn.
         /// </summary>
-        internal static List<Button> Buttons { get; } = new List<Button>();
+        public static List<Button> Buttons { get; } = new List<Button>();
 
         /// <summary>
         ///     Adds a button to the manager.
         /// </summary>
         /// <param name="btn"></param>
-        internal static void Add(Button btn) => Buttons.Add(btn);
+        public static void Add(Button btn) => Buttons.Add(btn);
 
         /// <summary>
         ///     Removes a button from the manager.
         /// </summary>
         /// <param name="btn"></param>
-        internal static void Remove(Button btn) => Buttons.Remove(btn);
+        public static void Remove(Button btn) => Buttons.Remove(btn);
     }
 }
