aa03ece2d83fb47ed84e464d8953da35a06ad615
Swan <me@swan.moe>
2019-05-05 15:30:52 -0400

    Revert testing text

--- Wobble.Tests/Screens/Selection/SelectionScreen.cs
+++ Wobble.Tests/Screens/Selection/SelectionScreen.cs
@@ -30,7 +30,7 @@ namespace Wobble.Tests.Screens.Selection
             {ScreenType.Primitives, "Primitives"},
             {ScreenType.ImGui, "Imgui"},
             {ScreenType.Scaling, "Scaling"},
-            {ScreenType.TextSizes, "memes"},
+            {ScreenType.TextSizes, "Text Sizes"},
         };
 
         public SelectionScreen() => View = new SelectionScreenView(this);
