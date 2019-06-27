099f7d4f2e4b1c1feb924078661bf2bc80636082
Swan <me@swan.moe>
2019-06-27 12:14:51 -0400

    Make NativeAssemblies public instead of internal

--- Wobble/Platform/NativeAssemblies.cs
+++ Wobble/Platform/NativeAssemblies.cs
@@ -2,37 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Wobble.Logging;
 
 namespace Wobble.Platform
 {
-    internal static class NativeAssemblies
+    public static class NativeAssemblies
     {
         /// <summary>
         ///     The architecture of the CPU.
         /// </summary>
-        internal static string Architecture => IntPtr.Size == 4 ? "x86" : "x64";
+        public static string Architecture => IntPtr.Size == 4 ? "x86" : "x64";
 
         /// <summary>
         ///     The base directory of the executable.
         /// </summary>
-        internal static string BaseDirectory => AppDomain.CurrentDomain.BaseDirectory;
+        public static string BaseDirectory => AppDomain.CurrentDomain.BaseDirectory;
 
         /// <summary>
         ///     The directory of all native libs.
         /// </summary>
-        internal static string NativeDirectory => $"{BaseDirectory}/{Architecture}/";
+        public static string NativeDirectory => $"{BaseDirectory}/{Architecture}/";
 
         /// <summary>
         ///     Copies native assemblies to the executing path
         /// </summary>
-        internal static void Copy()
+        public static void Copy()
         {
             foreach (var file in Directory.GetFiles(NativeDirectory))
             {
-                var path = Path.Combine(BaseDirectory, Path.GetFileName(file));
+                try
+                {
+                    var path = Path.Combine(BaseDirectory, Path.GetFileName(file));
 
-                if (!File.Exists(path))
-                    File.Copy(file, path, true);
+                    if (!File.Exists(path))
+                        File.Copy(file, path, true);
+                }
+                catch (Exception e)
+                {
+                    Logger.Error(e, LogType.Runtime);
+                }
             }
         }
     }
