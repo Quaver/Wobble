b047448829b82c6c15405464053b405c688b813a
Swan <me@swan.moe>
2019-08-08 12:53:04 -0400

    Add unit test project + localization tests

---
+++ Wobble.Tests.Unit/Localization/TestLocalization.cs
@@ -0,0 +1,84 @@
+using System;
+using Wobble.IO;
+using Wobble.Managers;
+using Xunit;
+
+namespace Wobble.Tests.Unit.Localization
+{
+    public class TestLocalization
+    {
+        private const string FolderPath = "Wobble.Tests.Resources/Localization";
+
+        [Fact]
+        public void TestGet()
+        {
+            Setup();
+
+            LocalizationManager.SetCurrentLanguage($"{FolderPath}/es.txt");
+            Assert.True(LocalizationManager.Get("Greeting") == "Hola");
+        }
+
+        [Fact]
+        public void TestGetEnum()
+        {
+            Setup();
+
+            LocalizationManager.SetCurrentLanguage($"{FolderPath}/es.txt");
+            Assert.True(LocalizationManager.Get(LocalizationStrings.Greeting) == "Hola");
+        }
+
+        [Fact]
+        public void TestOtherLanguage()
+        {
+            Setup();
+
+            LocalizationManager.SetCurrentLanguage($"{FolderPath}/fr.txt");
+            Assert.True(LocalizationManager.Get(LocalizationStrings.Greeting) == "Bonjour");
+        }
+
+        [Fact]
+        public void TestFallback()
+        {
+            Setup();
+
+            LocalizationManager.SetCurrentLanguage($"{FolderPath}/es.txt");
+
+            // Should fallback to English in this case
+            Assert.True(LocalizationManager.Get(LocalizationStrings.Bye) == "Goodbye");
+        }
+
+        [Fact]
+        public void TestNonExistent()
+        {
+            Setup();
+
+            LocalizationManager.SetCurrentLanguage($"{FolderPath}/es.txt");
+
+            try
+            {
+                var str = LocalizationManager.Get("Unknown_Key");
+            }
+            catch (Exception)
+            {
+                // All good. We want there to be an exception if the key was never found
+            }
+        }
+
+        private void Setup()
+        {
+            if (GameBase.Game != null)
+                return;
+
+            var game = new WobbleTestsGame();
+            game.Resources.AddStore(new DllResourceStore("Wobble.Tests.Resources.dll"));
+
+            LocalizationManager.SetDefaultLanguageFile($"{FolderPath}/en.txt");
+        }
+    }
+
+    public enum LocalizationStrings
+    {
+        Greeting,
+        Bye
+    }
+}
