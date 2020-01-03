6bfbf2322bf1b4d5e9e3d0451ef1cc21bb65e859
Swan <me@swan.moe>
2020-01-03 14:54:34 -0500

    Add initial mouse position offset to DraggableButton

--- Wobble/Graphics/UI/Buttons/DraggableButton.cs
+++ Wobble/Graphics/UI/Buttons/DraggableButton.cs
@@ -7,12 +7,24 @@ namespace Wobble.Graphics.UI.Buttons
 {
     public class DraggableButton : ImageButton
     {
+        private Vector2? GrabOffset { get; set; }
+
         /// <inheritdoc />
         /// <summary>
         /// </summary>
         /// <param name="image"></param>
         /// <param name="clickAction"></param>
-        public DraggableButton(Texture2D image, EventHandler clickAction = null) : base(image, clickAction) { }
+        public DraggableButton(Texture2D image, EventHandler clickAction = null) : base(image, clickAction)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (!IsHeld)
+                GrabOffset = null;
+
+            base.Update(gameTime);
+        }
 
         /// <inheritdoc />
         /// <summary>
@@ -21,8 +33,12 @@ namespace Wobble.Graphics.UI.Buttons
         /// <param name="gameTime"></param>
         protected override void OnHeld(GameTime gameTime)
         {
+            if (GrabOffset == null)
+                GrabOffset = new Vector2(MouseManager.CurrentState.X - AbsolutePosition.X, MouseManager.CurrentState.Y - AbsolutePosition.Y);
+
             Alignment = Alignment.TopLeft;
-            Position = new ScalableVector2(MouseManager.CurrentState.X, MouseManager.CurrentState.Y);
+
+            Position = new ScalableVector2(MouseManager.CurrentState.X - GrabOffset.Value.X, MouseManager.CurrentState.Y - GrabOffset.Value.Y);
         }
     }
 }
