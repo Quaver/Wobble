3500e9fba7a112ed0f8d2f3af7bda88f897673b9
Daniel Dimitrov <ayyayyedev@gmail.com>
2021-06-15 12:45:37 +0300

    Handle opening files under macOS

--- Wobble/Platform/OSX/OsxUtils.cs
+++ Wobble/Platform/OSX/OsxUtils.cs
@@ -1,13 +1,25 @@
+using System.Diagnostics;
+
 namespace Wobble.Platform.OSX
 {
     public class OsxUtils : Utils
     {
         public override void HighlightInFileManager(string path)
         {
+            var info = new ProcessStartInfo("open")
+            {
+                ArgumentList = {"-R", path}
+            };
+            Process.Start(info);
         }
 
         public override void OpenNatively(string path)
         {
+            var info = new ProcessStartInfo("open")
+            {
+                ArgumentList = {"-R", path}
+            };
+            Process.Start(info);
         }
 
         public override void RegisterURIScheme(string scheme, string friendlyName)
