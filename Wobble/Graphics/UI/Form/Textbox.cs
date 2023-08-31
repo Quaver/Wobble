2278bedba7469e048fba727517abe394f32952f7
Warp <warpaboi@gmail.com>
2023-08-31 12:02:51 +0200

    fix cursor wrong pos when !AllowCursorMovement

--- Wobble/Graphics/UI/Form/Textbox.cs
+++ Wobble/Graphics/UI/Form/Textbox.cs
@@ -69,6 +69,9 @@ namespace Wobble.Graphics.UI.Form
                     InputText.Alpha = 1;
                 }
 
+                if (!AllowCursorMovement)
+                    CursorPosition = RawText.Length;
+
                 ChangeCursorLocation();
             }
         }
