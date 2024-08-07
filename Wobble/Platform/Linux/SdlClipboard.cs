644482fd77b66387c654972a7cd7843a60f6eb79
Emik <emik.lakunishok@gmail.com>
2024-08-07 23:34:34 +0200

    Always trim null bytes

--- Wobble/Platform/Linux/SdlClipboard.cs
+++ Wobble/Platform/Linux/SdlClipboard.cs
@@ -2,8 +2,8 @@ namespace Wobble.Platform.Linux
 {
     public class SdlClipboard : Clipboard
     {
-        public override string GetText() => GameBase.Game.GetClipboardText();
+        public override string GetText() => GameBase.Game.GetClipboardText().TrimEnd('\0');
 
-        public override void SetText(string selectedText) => GameBase.Game.SetClipboardText(selectedText);
+        public override void SetText(string selectedText) => GameBase.Game.SetClipboardText(selectedText.TrimEnd('\0'));
     }
 }
