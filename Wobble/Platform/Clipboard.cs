88454629b0c8b01c2fba661958865c82677d1d9a
AiAe <ayyayyedev@gmail.com>
2026-07-13 19:42:16 +0300

    Fix clipboard on linux

--- Wobble/Platform/Clipboard.cs
+++ Wobble/Platform/Clipboard.cs
@@ -1,10 +1,15 @@
-﻿using TextCopy;
+﻿using System.Runtime.InteropServices;
+using TextCopy;
+using Wobble.Platform.Linux;
 
 namespace Wobble.Platform
 {
     public abstract class Clipboard
     {
-        public static Clipboard NativeClipboard { get; } = new TextCopyClipboard();
+        public static Clipboard NativeClipboard { get; } =
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+                ? (Clipboard)new SdlClipboard()
+                : new TextCopyClipboard();
 
         public abstract string GetText();
 
--- Wobble/Platform/Linux/SdlClipboard.cs
+++ Wobble/Platform/Linux/SdlClipboard.cs
@@ -1,11 +1,39 @@
-using TextCopy;
+using System;
+using System.Runtime.InteropServices;
 
 namespace Wobble.Platform.Linux
 {
     public class SdlClipboard : Clipboard
     {
-        public override string GetText() => (ClipboardService.GetText() ?? string.Empty).TrimEnd('\0');
+        private const string NativeLibrary = "mgruntime";
 
-        public override void SetText(string selectedText) => ClipboardService.SetText(selectedText.TrimEnd('\0'));
+        public override string GetText()
+        {
+            var text = SdlGetClipboardText();
+
+            if (text == IntPtr.Zero)
+                return string.Empty;
+
+            try
+            {
+                return (Marshal.PtrToStringUTF8(text) ?? string.Empty).TrimEnd('\0');
+            }
+            finally
+            {
+                SdlFree(text);
+            }
+        }
+
+        public override void SetText(string selectedText) => SdlSetClipboardText(selectedText.TrimEnd('\0'));
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_GetClipboardText")]
+        private static extern IntPtr SdlGetClipboardText();
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_SetClipboardText")]
+        [return: MarshalAs(UnmanagedType.I1)]
+        private static extern bool SdlSetClipboardText([MarshalAs(UnmanagedType.LPUTF8Str)] string text);
+
+        [DllImport(NativeLibrary, CallingConvention = CallingConvention.Cdecl, EntryPoint = "SDL_free")]
+        private static extern void SdlFree(IntPtr memory);
     }
 }
